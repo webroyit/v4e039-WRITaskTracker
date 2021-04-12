9ac9c139ae7ef8f25ac6f41e64962ff15c20b22c
royweb <xytank@gmail.com>
2021-04-12 14:16:19 -0400

    Add task component with styles

---
+++ src/components/Task.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="task">
+        <h3>{{ task.text }}</h3>
+        <p>{{ task.day }}</p>
+    </div>
+    
+</template>
+
+<script>
+
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    }
+}
+</script>
+
+<style scope>
+.task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+.task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
