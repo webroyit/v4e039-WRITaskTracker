1cc019b20b9cb2b242bd20c028574832b75d14c2
royweb <xytank@gmail.com>
2021-04-13 13:17:37 -0400

    Example of using $emit()

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="WRI Task Tracker" />
-    <Tasks :tasks="tasks" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
@@ -18,6 +18,11 @@ export default {
   data() {
     return {
       tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
     }
   },
   // This is a life cycle method
