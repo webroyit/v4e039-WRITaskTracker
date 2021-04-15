3f22040333f7710bd191bc3e54ed0be8ed71a586
royweb <xytank@gmail.com>
2021-04-15 13:32:30 -0400

    Add logic on add task component

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,14 +1,15 @@
 <template>
-    <form class="add-form">
+    <form @submit="onSubmit" class="add-form">
         <div class="form-control">
             <label>Task</label>
-            <input type="text" name="text" placeholder="Add Task" />
+            <input type="text" v-model="text" name="text" placeholder="Add Task" />
         </div>
 
         <div class="form-control">
             <label>Day & Time</label>
             <input
                 type="text"
+                v-model="day"
                 name="day"
                 placeholder="Add Day & Time"
             />
@@ -16,7 +17,7 @@
 
         <div class="form-control form-control-check">
             <label>Set Reminder</label>
-            <input type="checkbox"  name="reminder" />
+            <input type="checkbox" v-model="reminder" name="reminder" />
         </div>
 
         <input type="submit" value="Save Task" class="btn btn-block" />
@@ -26,7 +27,37 @@
 <script>
 
 export default {
-    name: 'AddTask'
+    name: 'AddTask',
+    data() {
+        return {
+            text: '',
+            day: '',
+            reminder: false
+        }
+    },
+    methods: {
+        onSubmit(e) {
+            e.preventDefault();
+
+            if (!this.text) {
+                alert('Please add a task');
+                return;
+            }
+
+            const newTask = {
+                id: Math.floor(Math.random() * 100000),
+                text: this.text,
+                day: this.day,
+                reminder: this.reminder
+            }
+
+            this.text = '';
+            this.day = '',
+            this.reminder = false;
+
+            this.$emit('add-task', newTask);
+        }
+    }
 }
 </script>
 
