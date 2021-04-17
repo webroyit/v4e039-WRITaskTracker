a76a2c54cb2dc86ddad466baaa13116315e8b923
royweb <xytank@gmail.com>
2021-04-17 12:03:15 -0400

    Create task on json-server

--- src/App.vue
+++ src/App.vue
@@ -29,8 +29,17 @@ export default {
     }
   },
   methods: {
-    addTask(task) {
-      this.tasks = [...this.tasks, task];
+    async addTask(task) {
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task) 
+      })
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data];
     },
     toggleAddTask(){
       this.showAddTask = !this.showAddTask;
